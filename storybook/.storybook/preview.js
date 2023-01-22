eebffcd6315bcab7c6cd8f56d70c04331846d87d
Pankaj Changra <pankaj.changra@simsaw.com>
2023-01-22 16:11:49 +0530

    Adding Chakra ui into the storybook

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,3 +1,15 @@
+import { addDecorator } from '@storybook/react';
+import { Box, ChakraBaseProvider, extendBaseTheme  } from '@chakra-ui/react'
+import chakraTheme from '@chakra-ui/theme'
+
+const { Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -11,3 +23,7 @@ export const parameters = {
       a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 }
+
+addDecorator(story => <ChakraBaseProvider theme={theme}>
+  <Box m='4'>{story()}</Box>
+</ChakraBaseProvider>)
