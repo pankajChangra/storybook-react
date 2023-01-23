972897d31271044701e00b8c25b9308c49f6447c
Pankaj Changra <pankaj.changra@simsaw.com>
2023-01-23 22:59:08 +0530

    addon-action

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-viewport",
+    "@storybook/addon-actions",
     "@storybook/preset-create-react-app"
   ],
   "framework": "@storybook/react",
--- storybook/src/components/ChakraButton/ChakraButton.stories.tsx
+++ storybook/src/components/ChakraButton/ChakraButton.stories.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from "@chakra-ui/button";
+import { action, actions} from "@storybook/addon-actions";
 
 export default {
     title: 'Chakra/Button',
     component: Button
 }
 
-export const Success = () => <Button colorScheme='green'>Success</Button>;
-export const Danger = () => <Button colorScheme='red'>Danger</Button>;
+export const Success = () => <Button onClick={action('Click Handler')} colorScheme='green'>Success</Button>;
+export const Danger = () => <Button {...actions('onClick', 'onMouseOver')}colorScheme='red'>Danger</Button>;
